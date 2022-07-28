7b48580e5a34f87af52c39a9ffa32cd80b3a4ddd
huytht <huytht2001@gmail.com>
2022-07-29 00:26:07 +0700

    set up routing and import some useful library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthenticationGuard } from './@guard/authentication.guard';
+import { RoleGuard } from './@guard/role.guard';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { UserComponent } from './pages/user/user.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AppConstant } from './@constant/app-constant';
+import { OrderComponent } from './pages/order/order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AdminLayoutComponent, canActivate: [AuthenticationGuard, RoleGuard], data: { roles: [`${AppConstant.APP_ROLE_ADMIN}`] }, children: [
+      { path: '', redirectTo: 'user', pathMatch: 'full' },
+      // { path: 'dashboard', component: DashboardComponent },
+      { path: 'user', component: UserComponent },
+      { path: 'order', component: OrderComponent },
+    ]
+  },
+  {
+    path: '', component: AuthLayoutComponent, children: [
+      { path: 'login', component: LoginComponent }
+    ]
+  },
+  { path: '**', redirectTo: 'user' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { AdminLayoutModule } from './layouts/admin-layout/admin-layout.module';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RoleGuard } from './@guard/role.guard';
+import { AuthInterceptor } from './@interceptors/auth.interceptor';
+import { AuthenticationGuard } from './@guard/authentication.guard';
+import { AuthLayoutModule } from './layouts/auth-layout/auth-layout.module';
+import { ToastrModule } from 'ngx-toastr';
+import { AppUtilService } from './@service/app-util.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminLayoutModule,
+    AuthLayoutModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      positionClass: 'toast-bottom-left',
+      preventDuplicates: true
+    })
   ],
-  providers: [],
+  providers: [
+    AuthenticationGuard,
+    RoleGuard,
+    AppUtilService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
