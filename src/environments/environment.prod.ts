35ab357af1aa6cd6fbdfcdd7f09fd30cbf142d2a
huytht <huytht2001@gmail.com>
2022-12-17 22:56:41 +0700

    change version package and change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://pet-store-api.onrender.com/api"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://localhost:7277/api"
+  apiUrl: "https://pet-store-api.onrender.com/api"
 };
 
 /*
